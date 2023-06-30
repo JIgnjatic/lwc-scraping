2eb74520f9f1054e049bff1b2e4b44c24922b4ea
Jovica <ignjaticjovica3@gmail.com>
2023-06-30 13:51:59 +0200

    [YLS-4] utility class

--- force-app/main/default/lwc/utilClass/utilClass.js
+++ force-app/main/default/lwc/utilClass/utilClass.js
@@ -1,29 +1,4 @@
 
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { ShowSpinnerEvent } from 'c/showSpinnerEvent';
-import { HideSpinnerEvent } from 'c/hideSpinnerEvent';
-
-
-
-export const showToast = (title, message, variant) => {
-    const event = new ShowToastEvent({
-        title: title,
-        message: message,
-        variant: variant
-    });
-    this.dispatchEvent(event);
-};
-
-export const showSpinner = () => {
-    const spinner = new ShowSpinnerEvent();
-    dispatchEvent(spinner);
-};
-
-export const hideSpinner = () => {
-    const spinner = new HideSpinnerEvent();
-    dispatchEvent(spinner);
-};
-
 export const validate = (template) =>{
     const allValid = [...template.querySelectorAll('lightning-input'), ...template.querySelectorAll('lightning-combobox')]
         .reduce((validSoFar, inputCmp) => {
@@ -33,7 +8,6 @@ export const validate = (template) =>{
     
     return allValid;
 };
-
 
 
 export * from './utilClass';
