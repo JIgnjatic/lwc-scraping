962d910fa8e059c5c6a9f61b84d20fc09ee370af
Jovica <ignjaticjovica3@gmail.com>
2023-07-03 21:39:38 +0200

    [YLS-1] adding duplication verification support and changed to one transaction per ticker

--- force-app/main/default/lwc/yahooFinanceScrapingCmp/yahooFinanceScrapingCmp.js
+++ force-app/main/default/lwc/yahooFinanceScrapingCmp/yahooFinanceScrapingCmp.js
@@ -3,10 +3,12 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import scrapeHTML from '@salesforce/apex/YahooFinanceScraping.scrapeHTML';
 import insertStockData from '@salesforce/apex/YahooFinanceScraping.insertStockData';
+import verifyDuplicates from '@salesforce/apex/YahooFinanceScraping.verifyDuplicates';
+import {validate} from 'c/utilClass';
 
 
-import SCRAPED_DATA from '@salesforce/schema/Scraped_Data__c';
-import TICKER_SYMBOL from '@salesforce/schema/Scraped_Data__c.Ticker_Symbol__c';
+import SCRAPED_DATA from '@salesforce/schema/Stock_Data__c';
+import TICKER_SYMBOL from '@salesforce/schema/Stock_Data__c.Ticker_Symbol__c';
 
 
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
@@ -15,30 +17,30 @@ import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 export default class YahooFinanceScrapingCmp extends LightningElement {
 
-	@track date = '2023-01-02';
-	dateWithOneMoreDay = '2023-01-03';
+	@track date = '2023-01-03';
+	dateWithOneMoreDay = '2023-01-04';
 	today = new Date().toISOString().slice(0,10);
 
 	title = 'Yahoo Finance Scraping';
 	@track ticker = '';
-	returnResult;
+	returnResult = [];
 	@track options;
 	selectedTickerList = [];
-	@track isLoading = false;
 
 	stockDataList = [];
 
 	@track showInputScreen = true;
 	@track showScrapeResults = false;
+	@track isLoading = false;
+
 
 	@track columns = [
-		{ label: 'ID', fieldName: 'id', type: 'text' },
 		{ label: 'Ticker', fieldName: 'ticker', type: 'text', innerWidth: 30 },
-		{ label: 'Date', fieldName: 'givenDate', type: 'date', typeAttributes: { year: 'numeric', month: '2-digit', day: '2-digit' }, innerWidth: 30 },
+		{ label: 'Industry ', fieldName: 'industry', type: 'text', innerWidth: 30 },
+		{ label: 'Date', fieldName: 'givenDate', type: 'date', innerWidth: 30 },
 		{ label: 'Open Price', fieldName: 'openPrice', type: 'number', innerWidth: 30 },
 		{ label: 'Close Price', fieldName: 'closePrice', type: 'number', innerWidth: 30 },
-		{ label: 'Market Cap', fieldName: 'marketCap', type: 'text', innerWidth: 30 },
-		{ label: 'Error', fieldName: 'errorMessage', type: 'text'}
+		{ label: 'Market Cap', fieldName: 'marketCap', type: 'text', innerWidth: 30 }
 	];
 
 	@track scrapedDataInsertResult = [];
@@ -69,39 +71,96 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 		}
 	}
 
-	handleScrapeButtonClick() {
+	async verifyDuplicates() {
+		console.log('verifying duplicates');
+		console.log(this.date);
 
-		console.log(this.selectedTickerList);
+		this.loading = true;
 
-		if(this.selectedTickerList.length == 0){
-			this.showErrorToast('Please select at least one ticker.');
-			return;
-		}
+		let duplicates = [];
+		
+		verifyDuplicates({ tickers: this.selectedTickerList, givenDateStr: this.date })
+		  	.then((result) => {
+				this.loading = false;
 
-		let dateInMiliseconds = this.convertDateToMiliseconds(this.date)/1000;		
-		let dateWithOneMoreDayInMiliseconds = this.convertDateToMiliseconds(this.dateWithOneMoreDay)/1000;
-
-		this.isLoading = true;
-		scrapeHTML({tickers: this.selectedTickerList, givenDateMiliseconds: dateInMiliseconds, givenDateMilisecondsPlusOneDay: dateWithOneMoreDayInMiliseconds})
-			.then(result => {
-				this.isLoading = false;
-				this.returnResult = result;
-
-				this.parseResult();
+				result = result.map((item) => {
+					console.log(item);
+					duplicates.push(item.Ticker_Symbol__);
+				});
 			})
-			.catch(error => {
+			.catch((error) => {
 				this.isLoading = false;
 				console.log('error: ' + error);
 				console.log('error: ' + JSON.stringify(error));
 				this.showErrorToast(error.body.message);
+				throw error; // rethrow the error to propagate it to the caller
 			});
+
+			return duplicates.length > 0;
+
+				
+				
+			
+	}
+
+
+	async handleScrapeButtonClick() {
+		let listBox = this.template.querySelector('lightning-dual-listbox');
+		
+		try {
+			//verifying if there are duplicates
+			if(await this.verifyDuplicates()){
+				this.showErrorToast('There are duplicate tickers for the selected date:'+ this.date);
+				listBox.setCustomValidity(duplicates + ' already have data for the ' + this.date + ' date.');
+				listBox.reportValidity();
+			}else{
+				listBox.setCustomValidity('');
+				listBox.reportValidity();
+				console.log('no duplicates');
+			}
+
+			//validating all input components for errors
+			if(!validate(this.template)){
+				this.showErrorToast('Please input all data');
+				return;
+			}
+
+			console.log('selected tickers: '+this.selectedTickerList);
+
+			this.isLoading = true;
+			for (let i = 0; i < this.selectedTickerList.length; i++) {
+				try {
+					let ticker = this.selectedTickerList[i];
+					let result = await this.scrapeHTML(ticker);
+
+					this.returnResult.push(result);
+				} catch (error) {
+					this.isLoading = false;
+					console.log('error: ' + error);
+					console.log('error: ' + JSON.stringify(error));
+					this.showErrorToast(error.body.message);
+				}
+			}
+
+			this.isLoading = false;
+			this.selectedTickerList = [];
+			this.scrapeResult();
+
+		}catch(error){
+			console.log(error);
+		}
     }
 
-	handleInputScreenButtonClick(){
-		this.isLoading = true;
-		this.showInputScreen = true;
-		this.showScrapeResults = false;
-		this.isLoading = false;
+	async scrapeHTML(ticker){
+
+		let dateInMiliseconds = this.convertDateToMiliseconds(this.date)/1000;		
+		let dateWithOneMoreDayInMiliseconds = this.convertDateToMiliseconds(this.dateWithOneMoreDay)/1000;
+
+		return await scrapeHTML({ticker:ticker,givenDateMiliseconds: dateInMiliseconds,givenDateMilisecondsPlusOneDay : dateWithOneMoreDayInMiliseconds})
+			.then(result => {
+				return result;
+			})
+
 	}
 
 	//method to insert stock data
@@ -122,24 +181,6 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 				this.showErrorToast(error.body.message);
 				this.stockDataList = [];
 			});
-	}
-
-	//showcase the results using stockDataList through lightning-datatable
-	showResults(){
-		this.isLoading = true;
-		this.showInputScreen = false;
-		this.showScrapeResults = true;
-		this.isLoading = false;
-	}
-
-	connectedCallback(){
-		console.log('connectedCallback');
-
-	}
-
-	handleTickerSelection(event) {
-		this.selectedTickerList = event.detail.value;
-		console.log('selectedTickerList: ' + this.selectedTickerList);
 	}
 
 
@@ -164,63 +205,79 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 		console.log('isWeekend: ' + isWeekend);
 		console.log('isHoliday: ' + isHoliday);
 
-
-		if(isWeekend || isHoliday || this.date > this.today){
+		if(isWeekend || isHoliday){
 			event.target.setCustomValidity('Please select a valid date.');
 			event.target.reportValidity();
+			this.template.querySelector('lightning-button.scrapeButton').disabled = true;
 		
 			//reset the date
 			this.date = null;
 			this.dateWithOneMoreDay = null;
+		}else{
+			event.target.setCustomValidity('');
+			event.target.reportValidity();
+
+			this.template.querySelector('lightning-button.scrapeButton').disabled = false;
 		}
 	}
 	
 
 	//method to parse result
-	parseResult(){
-		for(let i = 0; i < this.returnResult.length; i++){
+	scrapeResult(){
+		this.loading = true;
+		console.log('scraping...');
 
-			console.log(i);
+		try {
+			for(let i = 0; i < this.returnResult.length; i++){
 
-			let result = this.returnResult[i];
-
-			let stockDataHtml = result.stockData;
-			let companyInfoHtml = result.companyInfo;
-			let marketCapHtml = result.marketCap;
-
-			const parser = new DOMParser();
-			const stockDataDoc = parser.parseFromString(stockDataHtml, 'text/html');
-			const companyInfoDoc = parser.parseFromString(companyInfoHtml, 'text/html');
-			const marketCapDoc = parser.parseFromString(marketCapHtml, 'text/html');
-
-			let stockData = stockDataDoc.querySelector("section > div.Pb\\(10px\\).Ovx\\(a\\).W\\(100\\%\\) > table > tbody > tr:nth-child(1) > td.Py\\(10px\\).Ta\\(start\\).Pend\\(10px\\) > span");
-			let openPrice = stockDataDoc.querySelector("section > div.Pb\\(10px\\).Ovx\\(a\\).W\\(100\\%\\) > table > tbody > tr:nth-child(1) > td:nth-child(2) > span");
-
-			let closePrice = stockDataDoc.querySelector("section > div.Pb\\(10px\\).Ovx\\(a\\).W\\(100\\%\\) > table > tbody > tr:nth-child(1) > td:nth-child(5) > span");
-
-			let employeeNumber = companyInfoDoc.querySelector("section > div.asset-profile-container > div > div > p.D\\(ib\\).Va\\(t\\) > span:nth-child(8) > span");
-
-			let companyInfo = companyInfoDoc.querySelector("section > div.asset-profile-container > div > div > p.D\\(ib\\).W\\(47\\.727\\%\\).Pend\\(40px\\)");
-			const companyAddress = this.parseCompanyInfo(companyInfo.innerHTML);
-
-			let industry = companyInfoDoc.querySelector("section > div.asset-profile-container > div > div > p.D\\(ib\\).Va\\(t\\) > span:nth-child(5)");
-
-			let marketCap = marketCapDoc.querySelector("#quote-summary > div.D\\(ib\\).W\\(1\\/2\\).Bxz\\(bb\\).Pstart\\(12px\\).Va\\(t\\).ie-7_D\\(i\\).ie-7_Pos\\(a\\).smartphone_D\\(b\\).smartphone_W\\(100\\%\\).smartphone_Pstart\\(0px\\).smartphone_BdB.smartphone_Bdc\\(\\$seperatorColor\\) > table > tbody > tr:nth-child(1) > td.Ta\\(end\\).Fw\\(600\\).Lh\\(14px\\)");
-
-			this.stockDataList.push({
-				"ticker": result.ticker,
-				"givenDate": this.date,
-				"openPrice": openPrice.innerHTML,
-				"closePrice": closePrice.innerHTML,
-				"marketCap": marketCap.innerHTML,
-				"employeeNumber": employeeNumber.innerHTML,
-				"companyAddress": companyAddress,
-				"industry": industry.innerHTML
-			});
+				console.log(i);
+	
+				let result = this.returnResult[i];
+	
+	
+				let stockDataHtml = result.stockData;
+				let companyInfoHtml = result.companyInfo;
+				let marketCapHtml = result.marketCap;
+	
+				const parser = new DOMParser();
+				const stockDataDoc = parser.parseFromString(stockDataHtml, 'text/html');
+				const companyInfoDoc = parser.parseFromString(companyInfoHtml, 'text/html');
+				const marketCapDoc = parser.parseFromString(marketCapHtml, 'text/html');
+	
+				let stockData = stockDataDoc.querySelector("section > div.Pb\\(10px\\).Ovx\\(a\\).W\\(100\\%\\) > table > tbody > tr:nth-child(1) > td.Py\\(10px\\).Ta\\(start\\).Pend\\(10px\\) > span");
+				let openPrice = stockDataDoc.querySelector("section > div.Pb\\(10px\\).Ovx\\(a\\).W\\(100\\%\\) > table > tbody > tr:nth-child(1) > td:nth-child(2) > span");
+	
+				let closePrice = stockDataDoc.querySelector("section > div.Pb\\(10px\\).Ovx\\(a\\).W\\(100\\%\\) > table > tbody > tr:nth-child(1) > td:nth-child(5) > span");
+	
+				let employeeNumber = companyInfoDoc.querySelector("section > div.asset-profile-container > div > div > p.D\\(ib\\).Va\\(t\\) > span:nth-child(8) > span");
+	
+				let companyInfo = companyInfoDoc.querySelector("section > div.asset-profile-container > div > div > p.D\\(ib\\).W\\(47\\.727\\%\\).Pend\\(40px\\)");
+				const companyAddress = this.parseCompanyInfo(companyInfo.innerHTML);
+	
+				let industry = companyInfoDoc.querySelector("section > div.asset-profile-container > div > div > p.D\\(ib\\).Va\\(t\\) > span:nth-child(5)");
+	
+				let marketCap = marketCapDoc.querySelector("#quote-summary > div.D\\(ib\\).W\\(1\\/2\\).Bxz\\(bb\\).Pstart\\(12px\\).Va\\(t\\).ie-7_D\\(i\\).ie-7_Pos\\(a\\).smartphone_D\\(b\\).smartphone_W\\(100\\%\\).smartphone_Pstart\\(0px\\).smartphone_BdB.smartphone_Bdc\\(\\$seperatorColor\\) > table > tbody > tr:nth-child(1) > td.Ta\\(end\\).Fw\\(600\\).Lh\\(14px\\)");
+	
+				this.stockDataList.push({
+					"ticker": result.ticker,
+					"givenDate": this.date,
+					"openPrice": openPrice.innerHTML,
+					"closePrice": closePrice.innerHTML,
+					"marketCap": marketCap.innerHTML,
+					"employeeNumber": employeeNumber.innerHTML,
+					"companyAddress": companyAddress,
+					"industry": industry.innerHTML
+				});
+			}
+		} catch (error) {
+			console.log(JSON.stringify(error));
+			this.showErrorToast(error.body);
 		}
+		
+		this.loading = false;
+		this.returnResult = [];
 
 		this.insertStockData();
-
 	}
 
 
@@ -228,6 +285,14 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 	//
 	// HELPER METHODS
 	//
+
+
+	handleInputScreenButtonClick(){
+		this.isLoading = true;
+		this.showInputScreen = true;
+		this.showScrapeResults = false;
+		this.isLoading = false;
+	}
 
 
 	parseCompanyInfo(companyInfo){
@@ -242,6 +307,26 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 
 		return companyAddress;
 	}
+
+
+	//showcase the results using stockDataList through lightning-datatable
+	showResults(){
+		this.isLoading = true;
+		this.showInputScreen = false;
+		this.showScrapeResults = true;
+		this.isLoading = false;
+	}
+
+	connectedCallback(){
+		console.log('connectedCallback');
+
+	}
+
+	handleTickerSelection(event) {
+		this.selectedTickerList = event.detail.value;
+		console.log('selectedTickerList: ' + this.selectedTickerList);
+	}
+
 
 
 	//method to convert date to miliseconds
@@ -275,6 +360,7 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 		let isHoliday = false;
 		let holidays = [
 			'1/1',
+			'1/2',
 			'1/16',
 			'2/20',
 			'5/29',
