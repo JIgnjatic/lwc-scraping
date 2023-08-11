c30f5e953d524a150d416f18b62a5c9be31cd37b
Jovica <ignjaticjovica3@gmail.com>
2023-08-11 15:11:16 +0200

    [YLS-8] Adding check job status button and datatable

--- force-app/main/default/lwc/yahooFinanceScrapingCmp/yahooFinanceScrapingCmp.js
+++ force-app/main/default/lwc/yahooFinanceScrapingCmp/yahooFinanceScrapingCmp.js
@@ -4,6 +4,8 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import scrapeHTML from '@salesforce/apex/YahooFinanceScraping.scrapeHTML';
 import insertStockData from '@salesforce/apex/YahooFinanceScraping.insertStockData';
 import verifyDuplicates from '@salesforce/apex/YahooFinanceScraping.verifyDuplicates';
+import scrapeHTMLAsync from '@salesforce/apex/YahooFinanceScraping.scrapeHTMLAsync';
+import getJobsViaIDs from '@salesforce/apex/YahooFinanceScraping.getJobsViaIDs';
 import {validate} from 'c/utilClass';
 
 
@@ -17,6 +19,8 @@ import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 export default class YahooFinanceScrapingCmp extends LightningElement {
 
+	previousCloseDate = '2022-12-30';
+	previousCloseDatePlusOneDay = '2022-12-31';
 	@track date = '2023-01-03';
 	dateWithOneMoreDay = '2023-01-04';
 	today = new Date().toISOString().slice(0,10);
@@ -27,20 +31,32 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 	@track options;
 	selectedTickerList = [];
 
+	isAsync;
+	jobs = [];
+	jobResults = [];
+
 	stockDataList = [];
 
 	@track showInputScreen = true;
 	@track showScrapeResults = false;
 	@track isLoading = false;
+	@track showJobResult = false;
 
 
-	@track columns = [
+	@track stockColumns = [
 		{ label: 'Ticker', fieldName: 'ticker', type: 'text', innerWidth: 30 },
 		{ label: 'Industry ', fieldName: 'industry', type: 'text', innerWidth: 30 },
 		{ label: 'Date', fieldName: 'givenDate', type: 'date', innerWidth: 30 },
 		{ label: 'Open Price', fieldName: 'openPrice', type: 'number', innerWidth: 30 },
 		{ label: 'Close Price', fieldName: 'closePrice', type: 'number', innerWidth: 30 },
+		{ label: 'Previous Close Price', fieldName: 'previousClosePrice', type: 'number', innerWidth: 30 },
 		{ label: 'Market Cap', fieldName: 'marketCap', type: 'text', innerWidth: 30 }
+	];
+
+	@track jobColumns = [
+		{ label: 'Ticker', fieldName: 'ticker', type: 'text', innerWidth: 30 },
+		{ label: 'Status', fieldName: 'status', type: 'text', innerWidth: 30 },
+		{ label: 'Extended Status', fieldName: 'extendedStatus', type: 'text', innerWidth: 30 }
 	];
 
 	@track scrapedDataInsertResult = [];
@@ -97,6 +113,41 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 			return duplicates.length > 0;
 	}
 
+	asyncScrape(){
+
+		let previousCloseDateMiliseconds = this.convertDateToMiliseconds(this.previousCloseDate)/1000;
+		let previousCloseDateMilisecondsPlusOneDay = this.convertDateToMiliseconds(this.previousCloseDatePlusOneDay)/1000;
+
+		let dateInMiliseconds = this.convertDateToMiliseconds(this.date)/1000;		
+		let dateWithOneMoreDayInMiliseconds = this.convertDateToMiliseconds(this.dateWithOneMoreDay)/1000;
+
+		for (let i = 0; i < this.selectedTickerList.length; i++) {
+			let ticker = this.selectedTickerList[i];
+
+			scrapeHTMLAsync({ticker:ticker, givenDateMiliseconds:dateInMiliseconds, givenDateMilisecondsPlusOneDay:dateWithOneMoreDayInMiliseconds,
+							previousWorkDayMiliseconds: previousCloseDateMiliseconds, previousWorkDayMilisecondsPlusOneDay:previousCloseDateMilisecondsPlusOneDay})
+				.then(result =>{
+					let jobID = result;
+					console.log(jobID);
+					this.jobs.push(jobID);
+					this.jobResults.push({ticker: this.selectedTickerList[i], Id: jobID});
+
+					this.isLoading = false;
+				})
+				.catch (error =>{
+					this.isLoading = false;
+					console.log('error: ' + error);
+					console.log('error: ' + JSON.stringify(error));
+					this.showErrorToast(error.body.message);
+					this.stockDataList = [];		
+				})
+		}
+		this.showSuccessToast('Scrape job(s) enqueued successfully!');
+
+	}
+
+	
+
 	async handleScrapeButtonClick() {
 		let listBox = this.template.querySelector('lightning-dual-listbox');
 		
@@ -125,6 +176,12 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 			//todo:
 			//make callouts to send 4 tickers in a list to minimise duration
 			this.isLoading = true;
+
+			if(this.isAsync){
+				this.asyncScrape();
+				return;
+			}
+
 			for (let i = 0; i < this.selectedTickerList.length; i++) {
 				try {
 					let ticker = this.selectedTickerList[i];
@@ -186,8 +243,15 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 		let date = new Date(this.date);
 		date.setDate(date.getDate() + 1);
 		this.dateWithOneMoreDay = date.toISOString().slice(0,10);
+		date.setDate(date.getDate() - 2);
+
+		let previousWorkDay = this.getPreviousWorkDay(date);
+		this.previousCloseDate = previousWorkDay.toISOString().slice(0,10);
+		previousWorkDay.setDate(previousWorkDay.getDate() + 1);
+		this.previousCloseDatePlusOneDay = previousWorkDay.toISOString().slice(0,10);
 
 		console.log('date+1: ' + this.dateWithOneMoreDay);
+		console.log('previous close day: ' + this.previousCloseDate);
 
 		//get the date in milliseconds
 		let dateInMiliseconds = this.convertDateToMiliseconds(this.date);
@@ -274,6 +338,10 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 		this.insertStockData();
 	}
 
+	checkJobStatus(){
+
+		//query 
+	}
 
 
 	//
@@ -281,10 +349,60 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 	//
 
 
+	/**
+	 * Selects all tickers from the options variable and assigns them to the selectedTickerList variable.
+	 */
+	handleSelectAllClick() {
+		this.selectedTickerList = this.options.map(option => option.value);
+	}
+
+	handleDeselectAllClick() {
+		this.selectedTickerList = [];
+	}
+
+	//queries the jobIds and showcases the results of those Queueable jobs
+	handleCheckJobStatusClick() {
+
+		this.isLoading = true;
+
+		getJobsViaIDs({ jobIDs: this.jobs })
+			.then(result => {
+				this.jobResults = result.map(jobRes => {
+	
+					//get the matching job
+					const jobResID = jobRes.Id.slice(0,15);
+
+					const matchingJob = this.jobResults.find(job => {
+						return job.Id.slice(0,15) === jobResID;
+						
+					});
+
+					console.log('matchingJob ' +JSON.stringify(matchingJob));
+					if (matchingJob) {
+						return {
+							Id: jobRes.Id,
+							status: jobRes.Status,
+							extendedStatus: jobRes.ExtendedStatus,
+							ticker: matchingJob.ticker
+						};
+					}
+				});
+
+				this.showJobResults();
+				this.isLoading = false;
+			}).catch(error => {
+				this.isLoading = false;
+				console.log('error: ' + error);
+				console.log('error: ' + JSON.stringify(error));
+				this.showErrorToast(error.body.message);
+			})
+	}
+
 	handleInputScreenButtonClick(){
 		this.isLoading = true;
 		this.showInputScreen = true;
 		this.showScrapeResults = false;
+		this.showJobResult = false;
 		this.isLoading = false;
 	}
 
@@ -314,6 +432,19 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 		this.isLoading = false;
 	}
 
+	/**
+	 * Show the job results.
+	 */
+	showJobResults(){
+		this.isLoading = true;
+		this.showInputScreen = false;
+		this.showJobResult = true;
+		this.isLoading = false;
+	}
+
+	/**
+	 * Handles the selection of a ticker.
+	 */
 	handleTickerSelection(event) {
 		this.selectedTickerList = event.detail.value;
 		console.log('selectedTickerList: ' + this.selectedTickerList);
@@ -370,27 +501,68 @@ export default class YahooFinanceScrapingCmp extends LightningElement {
 		console.log('day: ' + day);
 		let dateString = month + '/' + day;
 
-
+		console.log(holidays.includes(dateString));
 		if(holidays.includes(dateString)){
-			isHoliday = true;
+			return true;
 		}
 
-		return isHoliday;
+		return false;
 	}
 
 	//isWeekend method
 	isWeekend(dateInMiliseconds){
-		let isWeekend = false;
 		let date = new Date(dateInMiliseconds);
 		let day = date.getDay();
 
 		console.log('day: ' + day);
 
 		if(day == 0 || day == 6){
-			isWeekend = true;
+			return true;
 		}
 
-		return isWeekend;
+		return false;
+	}
+
+	handleAsyncCheckbox(event){
+		this.isAsync = event.detail.checked;
+
+		if(!this.isAsync){
+			this.template.querySelector('lightning-button.checkJobStatus').disabled = true;
+		}else{
+			this.template.querySelector('lightning-button.checkJobStatus').disabled = false;
+		}
+	}
+
+ 	getPreviousWorkDay(date) {
+
+		let dateInMiliseconds = this.convertDateToMiliseconds(date.toISOString().slice(0,10));
+		console.log(dateInMiliseconds);
+
+		//check if the date is a weekend
+		let isWeekend = this.isWeekend(dateInMiliseconds);
+		
+		//check if the date is a US holiday
+		let isHoliday = this.isHoliday(dateInMiliseconds);
+		console.log('isWeekend: ' + isWeekend);
+		console.log('isHoliday: ' + isHoliday);
+		
+		while(!!isHoliday || !!isWeekend){	
+			date.setDate(date.getDate()-1);
+
+			dateInMiliseconds = this.convertDateToMiliseconds(date.toISOString().slice(0,10));
+
+			//check if the date is a weekend
+			isWeekend = this.isWeekend(dateInMiliseconds);
+			
+			//check if the date is a US holiday
+			isHoliday = this.isHoliday(dateInMiliseconds);
+
+			console.log('isWeekend: ' + isWeekend);
+			console.log('isHoliday: ' + isHoliday);
+			
+		}
+
+		return date;		
 	}
 
 
